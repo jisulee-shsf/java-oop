b94fcfc6b2bc8a29edaf2c2fac3167363f0b4610
Jisu Lee <jisulee.shsf@gmail.com>
2023-10-29 23:57:59 +0900

    Upload file

---
+++ chap_06/_04_Example.java
@@ -0,0 +1,16 @@
+package chap_06;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power() { /* 내용 생략 */ }
+    void channelUp() { /* 내용 생략 */ }
+    void channelDown() { /* 내용 생략 */ }
+}
+
+public class _04_Example {
+    public static void main(String[] args) {
+        Tv t = new Tv();
+    }
+}
