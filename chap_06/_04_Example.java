67458ea8288542401e6d2915471d82d34e45da04
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-06 18:24:14 +0900

    Update _04_Example.java

--- chap_06/_04_Example.java
+++ chap_06/_04_Example.java
@@ -1,5 +1,6 @@
 package chap_06;
 
+// 1) 클래스 정의
 class Tv {
     boolean power;
     int channel;
@@ -11,6 +12,7 @@ class Tv {
 
 public class _04_Example {
     public static void main(String[] args) {
+        // 2) 객체 생성
         Tv t = new Tv();
     }
 }
