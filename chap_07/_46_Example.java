abf5530af213ba56f732b10a7ee13a4962a0c245
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-12 22:40:27 +0900

    Upload _46_Example.java

---
+++ chap_07/_46_Example.java
@@ -0,0 +1,41 @@
+package chap_07;
+
+class Outer {
+    // 1) 인스턴스 내부 클래스
+    class InstanceInner {
+        int iv = 100;
+        // 1-1) 인스턴스 내부 클래스에서 static 변수를 선언하는 경우
+        // static int cv = 100; -> 에러 발생
+        // 1-2) 인스턴스 내부 클래스에서 static 상수를 선언하는 경우
+        final static int CONST = 100;
+    }
+
+    // 2) static 내부 클래스
+    static class StaticInner {
+        int iv = 200;
+        // 2-1) static 내부 클래스에서 static 변수를 선언하는 경우
+        static int cv = 200;
+        // 2-2) static 내부 클래스에서 static 상수를 선언하는 경우
+        final static int CONST = 200;
+    }
+
+    void method() {
+        // 3) 지역 내부 클래스
+        class LocalInner {
+            int iv = 300;
+            // 3-1) 지역 내부 클래스에서 static 변수를 선언하는 경우
+            // static int cv = 300; -> 에러 발생
+            // 3-2) 지역 내부 클래스에서 static 상수를 선언하는 경우
+            final static int CONST = 300;
+        }
+    }
+}
+
+public class _46_Example {
+    public static void main(String[] args) {
+        System.out.println(Outer.InstanceInner.CONST); // 100
+        System.out.println(Outer.StaticInner.CONST); // 200
+        // 3-3) 지역 내부 클래스의 static 상수를 사용하는 경우
+        // System.out.println(Outer.LocalInner.CONST); -> 에러 발생
+    }
+}
