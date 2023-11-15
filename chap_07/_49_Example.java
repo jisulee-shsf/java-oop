4d63b9345540a558f8196abda01c6f4c2dd606f5
Jisu Lee <jisulee.shsf@gmail.com>
2023-11-15 17:21:49 +0900

    Update _49_Example.java

--- chap_07/_49_Example.java
+++ chap_07/_49_Example.java
@@ -23,13 +23,13 @@ class Outer2 {
 
 public class _49_Example {
     public static void main(String[] args) {
-        // 5-1) 인스턴스 내부 클래스의 멤버에 접근하는 경우
+        // 5-1) 인스턴스 내부 클래스의 객체를 생성하는 경우
         // System.out.println(InstanceInner.iv); -> 에러 발생
         Outer2 o = new Outer2();
         Outer2.InstanceInner i = o.new InstanceInner();
         System.out.println(i.iv); // 100
 
-        // 5-2) static 내부 클래스의 멤버에 접근하는 경우
+        // 5-2) static 내부 클래스의 객체를 생성하는 경우
         // System.out.println(StaticInner.cv); -> 에러 발생
         System.out.println(Outer2.StaticInner.cv); // 200
         // System.out.println(Outer2.StaticInner.iv); -> 에러 발생
