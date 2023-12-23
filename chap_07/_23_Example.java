96fecf49eae3b33eee4f57e9c731f09041aa0e68
Jisu Lee <jisulee.shsf@gmail.com>
2023-12-23 12:01:45 +0900

    Update _23_Example.java

--- chap_07/_23_Example.java
+++ chap_07/_23_Example.java
@@ -16,8 +16,8 @@ class UpgradedPhone6 extends Phone5 {
 
 public class _23_Example {
     public static void main(String[] args) {
-        // 3) 참조 변수와 객체의 타입이 일치하는 경우
-        // 3-1) 자손 타입의 참조 변수로 같은 타입의 객체를 참조하는 경우
+        // 3) 참조 변수와 객체의 타입이 일치할 경우
+        // 3-1) 자손 타입의 참조 변수로 같은 타입의 객체를 참조할 경우
         UpgradedPhone6 u1 = new UpgradedPhone6();
         System.out.println(u1.name);
         System.out.println(u1.type);
@@ -27,7 +27,7 @@ public class _23_Example {
         u1.useNameDrop();
 
         // 4) 참조 변수와 객체의 타입이 일치하지 않는 경우
-        // 4-1) 조상 타입의 참조 변수로 자손 타입의 객체를 참조하는 경우
+        // 4-1) 조상 타입의 참조 변수로 자손 타입의 객체를 참조할 경우
         Phone5 p = new UpgradedPhone6();
         System.out.println(p.name);
         System.out.println(p.type);
@@ -36,7 +36,7 @@ public class _23_Example {
         // p.useStandBy(); -> 에러 발생
         // p.useNameDrop(); -> 에러 발생
 
-        // 4-2) 자손 타입의 참조 변수로 조상 타입의 객체를 참조하는 경우
+        // 4-2) 자손 타입의 참조 변수로 조상 타입의 객체를 참조할 경우
         // UpgradedPhone6 u2 = new Phone5(); -> 에러 발생
     }
 }
