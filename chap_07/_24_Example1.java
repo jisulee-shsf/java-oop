6e665c41824ef9a73f54dbbc96566f651cbed5b7
Jisu Lee <jisulee.shsf@gmail.com>
2023-12-23 12:02:04 +0900

    Update _24_Example1.java

--- chap_07/_24_Example1.java
+++ chap_07/_24_Example1.java
@@ -22,7 +22,7 @@ class UpgradedPhone8 extends Phone6 {
 
 public class _24_Example1 {
     public static void main(String[] args) {
-        // 3-1) 자손 타입의 참조 변수로 같은 타입의 객체를 참조하는 경우
+        // 3-1) 자손 타입의 참조 변수로 같은 타입의 객체를 참조할 경우
         UpgradedPhone7 u1 = new UpgradedPhone7();
         System.out.println(u1.name);
         System.out.println(u1.type);
@@ -31,7 +31,7 @@ public class _24_Example1 {
         u1.useStandBy();
         u1.useNameDrop();
 
-        // 3-2) 자손 타입의 참조 변수를 조상 타입의 참조 변수로 형변환하는 경우
+        // 3-2) 자손 타입의 참조 변수를 조상 타입의 참조 변수로 형변환할 경우
         Phone6 p = u1; // Phone6 p = (Phone6)u1;
         System.out.println(p.name);
         System.out.println(p.type);
@@ -40,7 +40,7 @@ public class _24_Example1 {
         // p.useStandBy(); -> 에러 발생
         // p.useNameDrop(); -> 에러 발생
 
-        // 3-3) 조상 타입의 참조 변수를 자손 타입의 참조 변수로 형변환하는 경우
+        // 3-3) 조상 타입의 참조 변수를 자손 타입의 참조 변수로 형변환할 경우
         UpgradedPhone7 u2 = (UpgradedPhone7)p;
         System.out.println(u2.name);
         System.out.println(u2.type);
@@ -49,7 +49,7 @@ public class _24_Example1 {
         u2.useStandBy();
         u2.useNameDrop();
 
-        // 3-4) 자손 타입의 참조 변수를 다른 자손 타입의 참조 변수로 형변환하는 경우
+        // 3-4) 자손 타입의 참조 변수를 다른 자손 타입의 참조 변수로 형변환할 경우
         // UpgradedPhone8 u3 = (UpgradedPhone8)u1; -> 에러 발생
     }
 }
